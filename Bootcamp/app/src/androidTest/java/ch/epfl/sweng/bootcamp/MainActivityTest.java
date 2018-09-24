9206628d5c9f6f1e62c933812bd2ee82dce525e2
Anne Donnet <anneviolon@gmail.com>
2018-09-24 19:18:19 +0200

    clean up app code

--- Bootcamp/app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
+++ Bootcamp/app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
